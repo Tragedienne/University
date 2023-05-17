9a896c2a13b9162a692df600717a5b5903d74902
Tragedienne <82025259+Tragedienne@users.noreply.github.com>
2023-05-17 21:15:31 +0300

    Add files via upload

---
+++ C#/L1/L1_1/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file name without .txt: ");
+            string File = Console.ReadLine();
+            StreamWriter writer = new StreamWriter(File + ".txt");
+
+            Random random= new Random();
+            int N = random.Next(10, 100);
+
+            writer.WriteLine(N);
+
+            for(int i = 0; i < N; i++)
+            {
+                writer.Write("{0:F3} ", - 100000.0 + random.NextDouble() * 200000);
+            }            
+            writer.Close();                   
+
+            StreamReader reader = new StreamReader(File + ".txt");
+
+            int count = int.Parse(reader.ReadLine());
+            string[] str_mas = reader.ReadLine().Split(' ');
+
+            double[] mas = new double[str_mas.Length];
+            for(int i = 0; i < count; i++)
+            {
+                mas[i] = double.Parse(str_mas[i]);
+            }
+            reader.Close();
+
+            Console.Write("Enter file name without .txt: ");
+            File = Console.ReadLine();
+            writer = new StreamWriter(File + ".txt");
+
+            var max = mas[0];
+            for(int i =0; i < N; i++)
+            {
+                if (mas[i] > max)
+                {
+                    max = mas[i];
+                }
+            }
+
+            var min = mas[0];
+            for (int i = 0; i < N; i++)
+            {
+                if (mas[i] < min)
+                {
+                    min = mas[i];
+                }
+            }
+
+            double avg = 0;
+            for (int i = 0; i < N; i++)
+            {
+                avg += mas[i];
+            }
+            avg/= N;
+
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                if (mas[i] > ((max + min) / 2))
+                {
+                    str.Append(mas[i] + "; ");
+                }
+            }           
+
+            writer.WriteLine("1. Среднее арифметическое максимального и минимального элементов: {0:F3} ", (max + min)/2);
+            writer.WriteLine("2. Среднее арифметическое всех элементов: {0:F3}", avg);
+            writer.WriteLine("3. Все числа, которые больше среднего арифметического максимального и минимального элементов: " + str.ToString());
+            writer.Close();
+
+        }
+    }
+}
---
+++ C#/L1/L1_2/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter file name without .txt: ");
+            string File = Console.ReadLine();
+            StreamWriter writer = new StreamWriter(File + ".txt");
+
+            Random random = new Random();
+            int N = random.Next(5, 10);
+            int M = random.Next(5, 10);
+
+            writer.WriteLine(N);
+            writer.WriteLine(M);
+            writer.Close();
+
+            StreamReader reader = new StreamReader(File + ".txt");
+            N = int.Parse(reader.ReadLine());
+            M = int.Parse(reader.ReadLine());
+            reader.Close();
+
+            Console.Write("Enter file name without .txt: ");
+            File = Console.ReadLine();
+            writer = new StreamWriter(File + ".txt");
+
+            int[,] mas = new int[N, M];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    mas[i, j] = random.Next(-1000, 1000);
+                    writer.Write(mas[i, j] + "\t");
+                }
+                writer.WriteLine();
+            }
+            writer.WriteLine();
+
+            double[] avg_mas = new double[M];
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    avg_mas[i] += mas[j, i];
+                }
+                avg_mas[i] /= N;
+                writer.WriteLine("Среднее арифметическое " + (i + 1) + " стобца: {0:F3}", avg_mas[i]);
+            }
+
+            writer.WriteLine();
+
+            char[,] char_mas = new char[N, M];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (mas[i, j] < avg_mas[j])
+                    {
+                        char_mas[i, j] = 'x';
+                    }
+                    else
+                    {
+                        char_mas[i, j] = 'y';
+                    }
+                    writer.Write(char_mas[i, j] + "\t");
+                }
+                writer.WriteLine();
+            }
+
+            writer.Close();
+        }
+    }
+}
