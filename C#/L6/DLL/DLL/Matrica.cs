99294964039410a5c144270a4f3bf6cf5ee61021
Tragedienne <82025259+Tragedienne@users.noreply.github.com>
2023-05-17 21:32:23 +0300

    Add files via upload

---
+++ C#/L6/DLL/DLL/Matrica.cs
@@ -0,0 +1,100 @@
+﻿namespace DLL
+{
+    public class Matrica
+    {
+        static int n, m;
+        static double[,] matrica;
+
+        static double[,] generate(int n)
+        {
+            Random random = new Random();
+            var matr = new double[n, n];
+            for (var i = 0; i < n; i++)
+                for (var j = 0; j < n; j++)
+                    matr[i, j] = random.NextDouble() * 10;
+            return matr;
+        }
+
+        static bool out_file(string name, string text)
+        {
+            using (var writer = new StreamWriter(name))
+            {
+                try
+                {
+                    writer.Write(text);
+                    return false;
+                }
+                catch (Exception e)
+                {
+                    return true;
+                }
+            }
+        }
+
+        static bool read(string name)
+        {
+            string[] lines;
+            try
+            {
+                lines = File.ReadAllLines(name);
+            }
+            catch
+            {
+                n = 5;
+                var str = "";
+                var matr = generate(n);
+                for (var i = 0; i < n; i++)
+                {
+                    var len = n - 1;
+                    for (var j = 0; j < len; j++)
+                        str += matr[i, j] + " ";
+                    str += matr[i, len] + "\n";
+                }
+                out_file(name, str);
+                lines = File.ReadAllLines(name);
+            }
+            n = lines.Length;
+            matrica = new double[n, n];
+            for (var i = 0; i < n; i++)
+            {
+                var s = lines[i].Split(' ');
+                m = s.Length;
+                if (n != m)
+                    return true;
+                try
+                {
+                    for (var j = 0; j < m; j++)
+                        matrica[i, j] = double.Parse(s[j]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool function(string input1, string input2, string output1)
+        {
+            if (read(input1))
+                return true;
+            double s1 = 0;
+            for (var i = 0; i < n; i++)
+                s1 += matrica[i, i] + matrica[i, n - i - 1];
+            if (n % 2 != 0)
+                s1 -= matrica[n / 2, n / 2];
+            if (read(input2))
+                return true;
+            double s2 = 0;
+            for (var i = 0; i < n; i++)
+                s2 += matrica[i, i] + matrica[i, n - i - 1];
+            if (n % 2 != 0)
+                s2 -= matrica[n / 2, n / 2];
+            if (out_file(output1, "The sum of the diagonal elements of the first matrix: " + s1 + '\n' +
+                "The sum of the diagonal elements of the second matrix: " + s2))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ C#/L6/L6/Program.cs
@@ -0,0 +1,26 @@
+﻿using DLL;
+namespace Lab6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the name of the first file with '.txt': ");
+            var input1 = Console.ReadLine();
+            while (input1.Length == 0)
+                input1 = Console.ReadLine();
+            Console.WriteLine("\nEnter the name of the second file '.txt': ");
+            var input2 = Console.ReadLine();
+            while (input2.Length == 0)
+                input2 = Console.ReadLine();
+            Console.WriteLine("\nEnter the name of the output file '.txt': ");
+            var output1 = Console.ReadLine();
+            while (output1.Length == 0)
+                output1 = Console.ReadLine();
+            if (Matrica.function(input1, input2, output1))
+                Console.WriteLine("\nThere is incorrect data in one or more of the specified files.");
+            else
+                Console.WriteLine("\nCompleted successfully))");
+        }
+    }
+}
