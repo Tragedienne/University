f09faeaf0f9f6a0ab363b7cceea8cf78175bb283
Tragedienne <82025259+Tragedienne@users.noreply.github.com>
2023-05-17 21:21:02 +0300

    Add files via upload

---
+++ C#/L3/Cylinder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3
+{
+    internal class Cylinder
+    {
+        private double R, H; 
+
+        public Cylinder(double r, double h)
+        {
+            try
+            {
+                R = r;
+                H = h;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public Cylinder()
+        {
+            R = 2; H = 2;
+            Console.WriteLine("Радиус по умолчанию: " + R + '\n' + "Высота по умолчанию: " + H);
+        }
+
+        public double GetV()
+        {
+            return Math.PI * Math.Pow(R, 2) * H;
+        }
+
+        public double GetS()
+        {
+            return 2 * Math.PI * R * H;
+        }
+
+        public static double GetV(double R, double H)
+        {
+            return Math.PI * Math.Pow(R, 2) * H;
+        }
+
+        public static double GetS(double R, double H)
+        {
+            return 2 * Math.PI * R * H;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Радиус и высота цилиндра: " + R + ' '+ H);
+            Console.WriteLine("Объем цилиндра: " + GetV());
+            Console.WriteLine("Площадь поверхности цилиндра: " + GetS());
+        }
+
+        public void Load()
+        {
+            try 
+            {
+                R = Convert.ToDouble(Console.ReadLine());
+                H = Convert.ToDouble(Console.ReadLine());
+            }   
+            catch (Exception e)
+            {
+                Console.WriteLine("Wrong type.");
+            }
+        }
+    }
+}
---
+++ C#/L3/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace L3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+#if !DEBUG
+            TextReader save_in = Console.In;
+            TextWriter save_out = Console.Out;
+            var reader = new StreamReader("in.txt");
+            var writer = new StreamWriter("out.txt");
+            Console.SetIn(reader);
+            Console.SetOut(writer);
+
+            Cylinder obj = new Cylinder();
+            obj.Info();
+            obj.GetV();
+            obj.GetS();
+
+            double r, h;
+
+            try
+            {
+                var str_mas = Console.ReadLine().Split(' ');
+                r = double.Parse(str_mas[0]);
+                h = double.Parse(str_mas[1]);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Incorrect value. Make random.");
+                r = new Random().NextDouble();
+                h = new Random().NextDouble();
+            }
+
+            Cylinder obj1 = new Cylinder(r, h);
+            obj1.Info();
+            obj1.GetV();
+            obj1.GetS();
+
+            Console.WriteLine("Объем статическим методом: " + Cylinder.GetV(r, h));
+            Console.WriteLine("Площадь статическим методом: " + Cylinder.GetS(r, h));
+
+            reader.Close();
+            writer.Close();
+#else
+            Cylinder obj = new Cylinder();
+            obj.Info();
+            obj.GetV();
+            obj.GetS();
+            
+            double r, h;
+
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter R and H in one line:");
+                    var str_mas = Console.ReadLine().Split(' ');
+                    r = double.Parse(str_mas[0]);
+                    h = double.Parse(str_mas[1]);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect value.");
+                }
+            }
+            
+            Cylinder obj1 = new Cylinder(r, h);
+            obj1.Info();
+            obj1.GetV();
+            obj1.GetS();
+
+            Console.WriteLine("Объем статическим методом: " + Cylinder.GetV(r, h));
+            Console.WriteLine("Площадь статическим методом: " + Cylinder.GetS(r, h));
+
+            Console.ReadKey();
+#endif
+        }
+    }
+}
