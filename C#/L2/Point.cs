84c4cfbe1b8e2f7d5e6e625d27557d050e6eae89
Tragedienne <82025259+Tragedienne@users.noreply.github.com>
2023-05-17 21:18:38 +0300

    Add files via upload

---
+++ C#/L2/Point.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2
+{
+    class Point
+    {
+        public double x, y, z;
+
+        public double GetToPoint()
+        {
+            double x1, y1, z1;
+#if DEBUG
+            Console.WriteLine("Enter x1, y1, z1: ");
+            while (true)
+            {
+                try
+                {
+                    x1 = Convert.ToDouble(Console.ReadLine());
+                    y1 = Convert.ToDouble(Console.ReadLine());
+                    z1 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong type. Try one more time.");
+                }
+            }
+#else
+            try
+            {
+                x1 = Convert.ToDouble(Console.ReadLine());
+                y1 = Convert.ToDouble(Console.ReadLine());
+                z1 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Wrong type.");
+                x1 = 0; y1 = 0; z1 = 0;
+            }
+            Console.WriteLine("Координаты x1, y1, z1: " + x1 + " " + y1 + " " + z1);
+#endif
+            return Math.Sqrt(Math.Pow(x1 - x, 2) + Math.Pow(y1 - y, 2) + Math.Pow(z1 - z, 2));
+        }
+
+        public double GetToNull()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Координыты точки в пространстве: " + x + " " + y + " " + z);
+            Console.WriteLine("Расстояние до введенной пользователем точки: " + GetToPoint());
+            Console.WriteLine("Расстояние до начала координат: " + GetToNull());
+        }
+
+        public void Load()
+        {
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            z = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ C#/L2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextReader t_in = Console.In;
+            TextWriter t_out = Console.Out;
+            var new_in = new StreamReader("in.txt");
+            var new_out = new StreamWriter("out.txt");
+            Console.SetIn(new_in);
+            Console.SetOut(new_out);
+#else
+            Console.WriteLine("Enter x, y, z: ");
+#endif
+            Point p = new Point();
+            p.Load();
+            p.Info();
+#if !DEBUG
+            Console.SetIn(t_in); new_in.Close();
+            Console.SetOut(t_out); new_out.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
