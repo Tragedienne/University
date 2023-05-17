569ef2ede333fd160f507e576c682a58d58c45fe
Tragedienne <82025259+Tragedienne@users.noreply.github.com>
2023-05-17 20:30:08 +0300

    Add files via upload

---
+++ WEB/LAB2/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $("#b1, #p1 but").click(function(){
+        $("#p1").slideToggle("fast");
+        return false;
+    });
+
+    $("#b2, #p2 but").click(function(){
+        $("#p2").slideToggle("fast");
+        return false;
+    });
+
+    $("#b3, #p3 but").click(function(){
+        $("#p3").slideToggle("fast");
+        return false;
+    });
+
+    $("#b4, #p4 but").click(function(){
+        $("#p4").slideToggle("fast");
+        return false;
+    });
+
+    $("#b5, #p5 but").click(function(){
+        $("#p5").slideToggle("fast");
+        return false;
+    });
+
+    $("#b6, #p6 but").click(function(){
+        $("#p6").slideToggle("fast");
+        return false;
+    });
+});
